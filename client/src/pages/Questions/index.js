d4a663377459293c4eabbebf2dcf33dc1cb1b0ae
Emrecan Balgün <deathlygrave@gmail.com>
2022-05-11 21:21:09 +0300

    add question subscription and update index.js and add loading component

--- client/src/pages/Questions/index.js
+++ client/src/pages/Questions/index.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useSubscription } from '@apollo/client';
+import Loading from '../../components/Loading';
+import { QUESTIONS_SUBSCRIPTIONS } from './queries';
+
+import { Link } from 'react-router-dom';
 
 function Questions() {
+    const { loading, data } = useSubscription(QUESTIONS_SUBSCRIPTIONS);
+
+    if(loading) {
+        return <Loading />
+    }
+
+    console.log(data);
+
   return (
-    <h1>Questions</h1>
+    <div>
+        {
+            data.questions.map((question) => (
+                <div key={question.id}>
+                    <Link to={`/q/${question.id}`}>{question.title}</Link>
+                </div>
+            ))
+        }
+    </div>
   )
 }
 
