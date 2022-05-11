4378f218ed2f66960b83722644e3088b555d0599
Emrecan Balgün <deathlygrave@gmail.com>
2022-05-11 15:54:55 +0300

    update apollo.js

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -1,9 +1,31 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
 
-const client = new ApolloClient({
-    uri: 'http://localhost:8080/v1/graphql',
-    cache: new InMemoryCache()
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:8080/v1/graphql',
+  options: {
+    reconnect: true,
+  },
 });
 
+const httpLink = new HttpLink({
+  uri: 'http://localhost:8080/v1/graphql',
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
 
 export default client;
