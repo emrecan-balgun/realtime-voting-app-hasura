9b01683094be4256bb58c96162ff0a82d6980847
Emrecan Balgün <deathlygrave@gmail.com>
2022-05-12 18:02:29 +0300

    update detail page and css

--- client/src/pages/Detail/index.js
+++ client/src/pages/Detail/index.js
@@ -7,55 +7,82 @@ import Error from '../../components/Error';
 
 function Detail() {
     const { id } = useParams();
-    const [selectedOptionId, setSelectedOptionId] = useState();
-
-    const { loading, error, data } = useSubscription(QUESTION_DETAIL_SUBSCRIPTION, {
-        variables: {
-            id
-        }
+    const [isVoted, setIsVoted] = useState(false);
+    const [selectedOptionId, setSelectedOptionId] = useState("");
+    const { loading, error, data } = useSubscription(
+      QUESTION_DETAIL_SUBSCRIPTION,
+      {
+        variables: { id },
+      }
+    );
+    const [newVote, { loading: loadingVote }] = useMutation(NEW_VOTE_MUTATION, {
+      onCompleted: () => setIsVoted(true),
     });
-
-    const [newVote, { loading: loadingVote }] = useMutation(NEW_VOTE_MUTATION);
-
+  
     const handleClickVote = () => {
-        newVote({
-            variables: {
-                input: {
-                    option_id: selectedOptionId,
-                },
-            },
-        });
+      newVote({
+        variables: {
+          input: {
+            option_id: selectedOptionId,
+          },
+        },
+      });
     };
-
-    if(loading) {
-        return <Loading />
+    if (loading) {
+      return <Loading />;
     }
-
-    if(error) {
-        return <Error message={error.message}/>
+    if (error) {
+      return <Error message={error.message} />;
     }
-
-    const { questions_by_pk: { options, title }} = data;
-
-  return (
+    const {
+      questions_by_pk: { options, title },
+    } = data;
+    const total = options.reduce(
+      (t, value) => t + value.votes_aggregate.aggregate.count,
+      0
+    );
+    return (
       <div>
-            <h2>{title}</h2>
-            {
-                options.map((option, i) => (
-                    <label htmlFor={i} key={i}>
-                        <input 
-                            type="radio" 
-                            name="selected" 
-                            value={option.id} 
-                            onChange={({ target }) => setSelectedOptionId(target.value)}
-                        />
-                        <span>{option.title}</span>
-                    </label>
-                ))
-            }
-            <button disabled={loadingVote} onClick={handleClickVote}>Vote</button>
+        <h2>{title}</h2>
+        {options.map((option, i) => (
+          <div key={i}>
+            <label htmlFor={i}>
+              <input
+                id={i}
+                type="radio"
+                name="selected"
+                value={option.id}
+                onChange={({ target }) => setSelectedOptionId(target.value)}
+              />
+              <span>{option.title}</span>
+              {isVoted && (
+                <span className="vote_count">
+                  (%
+                  {(
+                    (option.votes_aggregate.aggregate.count * 100) /
+                    (total === 0 ? 1 : total)
+                  ).toFixed(0)}
+                  )
+                </span>
+              )}
+            </label>
+            {isVoted && (
+              <div>
+                <progress
+                  value={option.votes_aggregate.aggregate.count}
+                  max={total}
+                />
+              </div>
+            )}
+          </div>
+        ))}
+        {!isVoted && (
+          <button disabled={loadingVote} onClick={handleClickVote}>
+            Vote
+          </button>
+        )}
       </div>
-  )
-}
-
-export default Detail
+    );
+  }
+  
+  export default Detail;
